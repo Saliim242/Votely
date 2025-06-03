b135dff94403fdf4b088e604f06afd4efb0acb82
Saliim242 <salim7442a@gmail.com>
2025-06-03 15:07:00 +0300

    Added nodmailer Configuration

---
+++ src/authentications/Emails/emails.js
@@ -0,0 +1,33 @@
+import {
+  VERIFICATION_EMAIL_TEMPLATE,
+  PASSWORD_RESET_REQUEST_TEMPLATE,
+  CONFITM_REQUEST_TEMPLATE,
+  PASSWORD_RESET_SUCCESS_TEMPLATE,
+} from "./email.templates.js";
+import transporter from "./nodmailer.config.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// Send verification Code email when the user is registored
+export const sendVerificationEmail = async (email, verificationCode) => {
+  const recepient = [{ email }];
+  try {
+    const mailOptions = {
+      from: `"Votely Support Team" <${process.env.EMAIL_USERNAME}>`,
+      to: recepient,
+      subject: "Verify Your Email",
+      html: VERIFICATION_EMAIL_TEMPLATE.replace(
+        "{verificationCode}",
+        verificationCode
+      ),
+    };
+
+    await transporter.sendMail(mailOptions);
+    console.log("Verification email sent successfully".green.bold);
+    //   return { success: true, messageId: info.messageId };
+  } catch (error) {
+    console.error("Error sending verification email:", error.red.bold);
+    throw new Error(`Error Sending verification Email ${error}`);
+  }
+};
---
+++ src/authentications/Emails/nodmailer.config.js
@@ -0,0 +1,17 @@
+import nodmailer from "nodmailer";
+import dotenv from "dotenv";
+
+// Config the The Environment Variables
+dotenv.config();
+
+// Create The Nodmailer Transporter
+
+export const transporter = nodmailer.createTransport({
+  host: process.env.EMAIL_HOST,
+  port: parseInt(process.env.EMAIL_PORT) || 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USERNAME,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
